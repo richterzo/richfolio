ac102aeecd8652b02cbf6c9b8998ba5e25a7204e
richterzo <riccardo.terzaghi1@gmail.com>
2020-12-01 19:38:18 +0100

    9-moody.mp3

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import { useWindowSize } from "@react-hook/window-size/throttled";
 import React from "react";
 //import { useWindowSize as useWindowSizeD } from "@react-hook/window-size/";
 //import useScrollPosition from "@react-hook/window-scroll";
-
+import Moody from "src/moody.mp3";
 function App() {
-
     const [width, height] = useWindowSize({ fps: 60 });
     //const [widthD, heightD] = useWindowSizeD();
     //const scrollY = useScrollPosition(60 /*frames per second*/);
-
     return (
+        <div>
         <iframe src="https://editor.p5js.org/richterzo/embed/tmX8UeKTq" width={width} height={height}/>
+        <audio
+            ref="audio_tag"
+            autoPlay={true}
+            controls={true} >
+            <source type="audio/mp3" src={Moody} />
+        </audio>
+        </div>
     );
 }
 
