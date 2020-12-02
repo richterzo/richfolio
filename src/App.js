fc3a1108d09b7d99675c21e0500bae8065463e2e
richterzo <riccardo.terzaghi1@gmail.com>
2020-12-03 00:15:39 +0100

    15- TRY FIX 3 mp3 don't play

--- src/App.js
+++ src/App.js
@@ -4,25 +4,19 @@ import React from "react";
 //import { useWindowSize as useWindowSizeD } from "@react-hook/window-size/";
 //import useScrollPosition from "@react-hook/window-scroll";
 import track1 from "./assets/tracks/moody.mp3";
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
+import silence from "./assets/tracks/250-milliseconds-of-silence.mp3";
 
 function App() {
     const [width, height] = useWindowSize({ fps: 60 });
-    const tracks = [{
-        img:'https://icon-library.net/images/music-icon-transparent/music-icon-transparent-11.jpg',
-        name:'MOODY',
-        desc:'TRACK1',
-        src:track1}];
     //const [widthD, heightD] = useWindowSizeD();
     //const scrollY = useScrollPosition(60 /*frames per second*/);
     return (
         <div>
+            <iframe id="trigger" src={silence} allow="autoplay"/>
+            <audio id="trigger" autoPlay loop>
+                <source src={track1} type="audio/mp3"></source>
+            </audio>
             <iframe src="https://editor.p5js.org/richterzo/embed/tmX8UeKTq" width={width} height={height}/>
-            <AudioPlayer
-                autoPlay
-                src={track1}
-                onPlay={e => console.log("onPlay")}/>
         </div>
     );
 }
